eafa3546665fdf5c217cbc72ccb33ab30776d121
shikajiko <riyanfadli6675@gmail.com>
2025-10-13 19:06:11 +0700

    remove buggy mail php

--- Pertemuan 7/ajax.php
+++ Pertemuan 7/ajax.php
@@ -1,27 +1,17 @@
 <?php
-if ( ($_POST['name']!="") && ($_POST['email']!="")){
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+if (!empty($_POST['name']) && !empty($_POST['email'])) {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+	
+    sleep(1);
 
-$to = "riyanfadli675@gmail.com";
-$subject = "PHP Email with AJAX";
-$message = "<p>New email is received from $name.</p>
-<p>$message</p>";
-
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-$headers .= "From: <".$email.">" . "\r\n";
-$sent = mail($to,$subject,$message,$headers);
-if($sent){
-	echo "<span style='color:green; font-weight:bold;'>
-	Submission accepted.
-	</span>";
-}
-else{
-	echo "<span style='color:red; font-weight:bold;'>
-	Something went wrong.
-	</span>";
-	}
+    echo "<span style='color:green; font-weight:bold;'>
+    Hi, $name! Your message has been received successfully (simulation only, no email sent).
+    </span>";
+} else {
+    echo "<span style='color:red; font-weight:bold;'>
+    Error: Missing name or email field.
+    </span>";
 }
 ?>
